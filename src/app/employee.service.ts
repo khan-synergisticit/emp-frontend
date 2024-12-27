cefe856bb2e0ba76359098c187f6fad64abb1195
donkey <khansession55@gmail.com>
2024-12-27 22:13:48 +0000

    last

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -8,7 +8,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseUrl = "http://10.128.0.6:8020/api/v1/employees";
+  private baseUrl = "http://10.96.0.13:8020/api/v1/employees";
   private header = new HttpHeaders().set("Access-Control-Allow-Origin", "*");
 
   constructor(private httpclient: HttpClient) { }
